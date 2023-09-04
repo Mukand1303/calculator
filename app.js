e020577f201e65d81ba65bcf8d0f46907c3aad77
Mukand Kirshana <mukandkirshana1606@gmail.com>
2023-09-04 19:46:49 +0500

    done

---
+++ app.js
@@ -0,0 +1,18 @@
+
+function clck(e){
+    document.getElementById("inp").value += e
+}
+
+function eq(){
+    var a = document.getElementById("inp")
+    a.value = eval(a.value)
+}
+
+function clr(){
+    document.getElementById("inp").value = ""
+}
+
+function del(){
+  var a =  document.getElementById("inp")
+    a.value = a.value.slice(0,-1)
+}
